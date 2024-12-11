6116e6609b90d0c30d0f755e98b30172bfaae268
Harsh Rohila <harsh.rohila@procore.com>
2024-12-11 09:07:23 +0530

    removed button

--- procore/src/App.tsx
+++ procore/src/App.tsx
@@ -1,17 +1,7 @@
 import "./App.css";
-import { Button } from "@procore/core-react";
 
 function App() {
-  return (
-    <div className="App">
-      <Button>hello</Button>
-      {/* <ShipmentsTable shipments={shipmentData} /> */}
-      {/* <Datatable>
-        <Column field="name" headerName="NAME" />
-        <Column field="id" headerName="ID" />
-      </Datatable> */}
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
