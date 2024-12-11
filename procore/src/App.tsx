35acfd8e12e761f8002c0790d0248df95689f84b
hariramdhaka <hari.ram@procore.com>
2024-12-11 15:28:26 +0530

    removed extra test code

--- procore/src/App.tsx
+++ procore/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { SignInScreen } from "./components/auth14/SignInScreen";
+import { SignInScreen } from "./components/auth_chakra/SignInScreen";
 
 function App() {
   return (
